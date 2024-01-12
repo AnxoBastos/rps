b6a2b76d15bb868d2517040c2c1f0f1009610fe1
AnxoBastos <anxoclase@gmail.com>
2024-01-12 13:08:39 +0100

    feat: add PreviousMoveEngine and PredictiveEngine
    add PreviousMoveEngine and PredictiveEngine (future explanation on README)

--- src/engines.py
+++ src/engines.py
@@ -46,3 +46,53 @@ class TenMovesEngine(ActionEngine):
             most_frequent = GameAction.Rock
 
         return Victories[most_frequent]
+    
+class PreviousMoveEngine(ActionEngine):
+
+    def __init__(self):
+        super().__init__()
+
+    def generate_action(self):
+        if not self._opponent_history:
+            self._opponent_history.append(GameAction.Rock)
+
+        return Victories[self._opponent_history[-1]]
+
+class PredictiveEngine(ActionEngine):
+    
+    def __init__(self):
+        self._play_order = {
+                (GameAction.Rock, GameAction.Rock) : 0, 
+                (GameAction.Rock, GameAction.Paper) : 0, 
+                (GameAction.Rock, GameAction.Scissors) : 0, 
+                (GameAction.Paper, GameAction.Rock) : 0, 
+                (GameAction.Paper, GameAction.Paper) : 0, 
+                (GameAction.Paper, GameAction.Scissors) : 0,
+                (GameAction.Scissors, GameAction.Rock) : 0, 
+                (GameAction.Scissors, GameAction.Paper) : 0, 
+                (GameAction.Scissors, GameAction.Scissors) : 0
+            }
+        super().__init__()
+
+    def generate_action(self):
+        if not self._opponent_history:
+            self._opponent_history.append(GameAction.Rock)
+
+        last_two = tuple(self._opponent_history[-2:])
+        if len(last_two) == 2:
+            self._play_order[last_two] += 1
+
+        potential_plays = [
+            (self._opponent_history[-1], GameAction.Rock),
+            (self._opponent_history[-1], GameAction.Paper),
+            (self._opponent_history[-1], GameAction.Scissors)
+        ]
+
+        sub_order = {
+            play: self._play_order[play]
+            for play in potential_plays
+        }
+
+        prediction = max(sub_order, key=sub_order.get)[-1]
+
+        return Victories[prediction]
