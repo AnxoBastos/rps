4ef344a6eadb9cedbd11aceea61076b2bb49ada0
AnxoBastos <anxoclase@gmail.com>
2024-01-12 12:56:08 +0100

    feat: add RandomEngine and TenMovesEngine
    add RandomEngine and TenMovesEngine (future explanation on README)

--- src/engines.py
+++ src/engines.py
@@ -22,3 +22,27 @@ class ActionEngine(metaclass=ABCMeta):
 
     def add_opponent_action(self, action):
         self._opponent_history.append(action)
+
+class RandomEngine(ActionEngine):
+    
+    def __init__(self):
+        super().__init__()
+
+    def generate_action(self):
+            computer_selection = random.randint(0, len(GameAction) - 1)
+            computer_action = GameAction(computer_selection)
+            return computer_action
+
+class TenMovesEngine(ActionEngine):
+    
+    def __init__(self):
+        super().__init__()
+
+    def generate_action(self):
+        last_ten = self._opponent_history[-10:]
+        most_frequent = max(set(last_ten), default=None, key=last_ten.count)
+
+        if most_frequent == None:
+            most_frequent = GameAction.Rock
+
+        return Victories[most_frequent]
